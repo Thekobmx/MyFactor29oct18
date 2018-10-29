1508d40b605f7e281917ac64bf576e165bf845ca
Thekob <thekob.mx@gmail.com>
2018-10-29 11:32:49 +0700

    Initial Commit

---
+++ MyFactor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyFactor
+//
+//  Created by kotchaphan on 29/10/2561 BE.
+//  Copyright © 2561 Thanapat.P. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
